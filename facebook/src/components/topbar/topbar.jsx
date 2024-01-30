5a4d2b5ffc9d418e57a4d78d831389db2f40da5f
Gehendra1 <chaudharygehendra46@gmail.com>
2024-01-30 13:31:25 +0545

    commkit

--- facebook/src/components/topbar/topbar.jsx
+++ facebook/src/components/topbar/topbar.jsx
@@ -42,7 +42,7 @@ export default function Topbar() {
         <div className="middleSectionTopbar">
           <ul>
             <Link
-              to="/Facebook"
+              to="/"
               className={`${
                 isActiveLink("/Facebook") ? "active " : ""
               }responsiveTopbar`}
